aba4fd58e7b7fd700cd72950df04a7e98497c16a
JelteBoumansAP <94611154+JelteBoumansAP@users.noreply.github.com>
2023-04-12 15:56:03 +0200

    Add files via upload

--- Firmware/Datalogger_communicatie_test/Datalogger_communicatie_test.ino
+++ Firmware/Datalogger_communicatie_test/Datalogger_communicatie_test.ino
@@ -1,9 +1,18 @@
-String jsonData = "{\"temp\": 28,\"humidity\": 75}";
+String jsonData = "";
 
 void setup() {
   Serial.begin(9600); // initialize serial communication
   pinMode(A0, OUTPUT);
   digitalWrite(A0, LOW);
+  pinMode(A1, INPUT);
+  randomSeed(analogRead(A1));
+
+  int temp = random(12,32);
+  int temp_kast = random(12,32);
+  int humidity = random(70,95);
+  int dew_point = temp-((100-humidity)/5);
+
+  jsonData = "{\"temp\": "+String(temp)+",\"temp_kast\": "+String(temp_kast)+",\"humidity\": "+String(humidity)+",\"dew_point\": "+String(dew_point)+"}";
 }
 
 void loop() {
